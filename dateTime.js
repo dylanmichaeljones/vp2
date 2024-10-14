19651ee6dff012c270541fa26868be4d187cc249
Dylan Michael Jones <dylanmichaeljones@users.noreply.github.com>
2024-10-14 11:55:06 +0300

    Filmitegelaste andmebaasi päring lisatud

--- dateTime.js
+++ dateTime.js
@@ -1,18 +1,19 @@
 const weekDayNamesEt = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
 const monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+
 const dateFormattedEt = function(){
 	let timeNow = new Date();
 	let dateNow = timeNow.getDate();
 	let monthNow = timeNow.getMonth();
 	let yearNow = timeNow.getFullYear();
 	return dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow;
-}
+};
 
 const weekDayEt = function(){
 	let timeNow = new Date();
-	let dayNow = timeNow.getDay();
-	return dayNamesEt[dayNow];
-}
+	let weekDayNow = timeNow.getDay();
+	return weekDayNamesEt[weekDayNow];
+};
 
 const timeFormattedEt = function(){
 	let timeNow = new Date();
@@ -21,52 +22,52 @@ const timeFormattedEt = function(){
 	let secondsNow = timeNow.getSeconds();
 	let timeEt = hoursNow + ":" + minutesNow + ":" + secondsNow;
 	return timeEt	
-}
+};
 
-//const partOfDay = function(){
+/*const partOfDay = function(){
 	let dPart = "suvaline aeg";
 	let hourNow = new Date().getHours();
-	if(dayNow = "esmaspäev"){
+	if(weekDayNow = "esmaspäev"){
 		if(hourNow > 8 && hourNow <= 16){
 		dPart = "Kooliaeg!";
 	} else if(hourNow > 16 && hourNow <= 0o0){
 		dPart = "Kool läbi tänaseks :(";
 	}
 }
-	if(dayNow = "teisipäev"){
+	if(weekDayNow = "teisipäev"){
 		if(hourNow > 10 && hourNow <= 12){
 		dPart = "Kooliaeg!";
 	} else if(hourNow > 12 && hourNow <= 0o0){
 		dPart = "Kool läbi tänaseks :(";
 	}
 }
-	if(dayNow ="kolmapäev"){
+	if(weekDayNow ="kolmapäev"){
 		if(hourNow > 8 && hourNow <= 14){
 			dPart = "Kooliaeg!";
 	} else if(hourNow > 14 && hourNow <= 0o0){
 		dPart = "Kool läbi tänaseks :(";
 	}
 }
-	if(dayNow = "neljapäev"){
+	if(weekDayNow = "neljapäev"){
 		if(hourNow > 10 && hourNow <= 12){
 			dPart = "Kooliaeg!";
 	} else if(hourNow > 12 && hourNow <= 0o0){
 			dPart = "Kool läbi tänaseks :(";
 	}
 }
-	if(dayNow = "reede"){
+	if(weekDayNow = "reede"){
 		dPart = "Täna pole kooli, tee midagi toredat!";
 }
-	if(dayNow = "pühapäev, laupäev"){
+	if(weekDayNow = "pühapäev, laupäev"){
 		return "Jee, nädalavahetus, jõuab palju õppida!";
 }
 	if(hourNow >0o0 && hourNow <= 7){
 		dPart = "Tuduaeg, ilusaid unenägusid!";
 }
 return dPart;
-
-
-console.log(partOfDay());
+}
+*/
+//console.log(partOfDay());
 
 //ekspordin kõik vajaliku
-module.exports = {dateFormattedEt: dateFormattedEt, weekDayEt: weekDayEt, timeFormattedEt: timeFormattedEt, weekDayNamesEt: weekDayNamesEt, monthNamesEt: monthNamesEt, dayPart: partOfDay};
+module.exports = {dateFormattedEt: dateFormattedEt, weekDayEt: weekDayEt, timeFormattedEt: timeFormattedEt, weekDayNamesEt: weekDayNamesEt, monthNamesEt: monthNamesEt};
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const dateTime = require("./dateTime");
 const fs = require("fs");
 //et saada k6ik p2ringud k2tte
 const bodyparser = require("body-parser");
+//andmebaasi andmed
+const dbInfo = require("../../vp2024config");
+//andmebaasiga suhtlemine
+const mysql = require("mysql2");
 
 const app = express();
 
@@ -13,16 +17,25 @@ app.use(express.static("public"));
 //kasutame body-parserit p2ringute parsimiseks (kui ainult tekst, siis false, kui ka pildid jms, siis true
 app.use(bodyparser.urlencoded({extended: false}));
 
+//loon andmebaasiyhenduse
+const conn = mysql.createConnection({
+	host: dbInfo.configData.host,
+	user: dbInfo.configData.user,
+	password: dbInfo.configData.passWord,
+	database: dbInfo.configData.dataBase
+});
+
 app.get("/", (req, res)=>{
 	//res.send("Express läks käima!");
+	//console.log(dbInfo.configData.host);
 	res.render("index");
 });
 
 app.get("/timenow", (req, res)=>{
-	const weekDayNow = dateTime.weekDayEt();
 	const dateNow = dateTime.dateFormattedEt();
+	const weekDayNow = dateTime.weekDayEt();
 	const timeNow = dateTime.timeFormattedEt();
-	res.render("timeNow",{nowWD: weekdayNow, nowD: dateNow, notT: timeNow
+	res.render("timenow",{nowWD: weekDayNow, nowD: dateNow, nowT: timeNow
 	});
 });
 
@@ -64,6 +77,26 @@ app.post("/regvisit", (req, res)=>{
 		}
 	});
 	//res.render("regvisit");
+});
+
+app.get("/eestifilm", (req, res)=>{
+	res.render("eestifilm");
+});
+
+app.get("/eestifilm/tegelased", (req, res)=>{
+	//loond andmebaasip2ringu
+	let sqlReq = "SELECT first_name, last_name, birth_date FROM person";
+	conn.query(sqlReq, (err, sqlRes)=>{
+		if(err){
+		res.render("tegelased", {persons: {first_name: "Ei", last_name: "leidnud", birth_date: "VIGA"}});
+			//throw err;
+		}
+		else {
+			//console.log(sqlRes);
+			res.render("tegelased", {persons: sqlRes});
+		}
+	});
+	//res.render("tegelased");
 });
 
 app.listen(5256);
